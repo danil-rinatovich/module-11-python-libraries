83394487003ffa6d7d133b77dc3abc4c82c493ef
danil_rinatovich <danil_rinatovich@list.ru>
2024-12-24 00:33:30 +0500

    changed logic

--- module_11_1.py
+++ module_11_1.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageFilter
 2. Изменения размера изображения
 3. Изменения формата с JPEG на PNG
 4. Обработка изображения при помощи модуля ImageFilter
-5. Объединения двух изображений в одно"""
+5. Объединение изображений в одно"""
 
 small = Image.open('small_silva.jpg')
 big = Image.open('big_silva.jpg')
@@ -16,17 +16,18 @@ print(small.size, big.size, small.format, big.format, small.mode, big.mode)
 big_size = (1280, 960)
 big.thumbnail(big_size)
 
-small.save('change_size_small.jpg', 'png')
-big.save('change_size_big.jpg', 'png')
-change_size_small = Image.open('change_size_small.png')
-change_size_big = Image.open('change_size_big.png')
+small.save('change_format_small.png')
+big.save('change_format_big.png')
 
-print(change_size_small.size, change_size_big.size,
-      change_size_small.format, change_size_big.format,
-      change_size_small.mode, change_size_big.mode)
+change_format_small = Image.open('change_format_small.png')
+change_format_big = Image.open('change_format_big.png')
 
-big_silva_filter = big.filter(ImageFilter.DETAIL)
+print(change_format_small.size, change_format_big.size,
+      change_format_small.format, change_format_big.format,
+      change_format_small.mode, change_format_big.mode)
+
 small_silva_filter = small.filter(ImageFilter.SMOOTH_MORE)
+big_silva_filter = big.filter(ImageFilter.DETAIL)
 
 img = Image.blend(big_silva_filter, small_silva_filter, 0.6)
 img.save('img.jpg')
